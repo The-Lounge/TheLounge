27b6fafeda7e961257018adfb2e53c6632d21996
Greg Rozmarynowycz <crashtest1@live.com>
2017-02-26 16:15:41 -0500

    update deploy script because app folder is gone

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo 'create deploy artifact'
-zip -r dist.zip .elasticbeanstalk app Gruntfile.js package.json
+zip -r dist.zip .elasticbeanstalk .tmp api config mocks ng views app.js Gruntfile.js package.json

2c54a9ab54914ffe9f546a809e943ff36e0db3c3
Greg Rozmarynowycz <crashtest1@live.com>
2017-03-07 18:49:10 -0500

    API create posting integration test

--- test/api-int/posting.js
+++ test/api-int/posting.js
@@ -5,31 +5,74 @@
 require('any-promise/register/q');
 const request = require('request-promise-any');
 const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
+chai.use(require('chai-as-promised'));
 
 const expect = chai.expect;
 
 const API_PATH = 'http://localhost:1337/api/';
 
-const expectedPosting = {
-  seller_id: '-6',
-  id: '-1',
-  title: '$5 Nail Painting',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed ipsum nisl',
-  active: true,
-  category: {
-    shortname: 'beauty',
-    name: 'Beauty',
+const testData = {
+  createPosting: {
+    seller_id: '-5',
+    title: 'This is a test posting',
+    description: 'some posting description text',
+    category_id: 3,
+  }
+};
+
+const expected = {
+  getPosting: {
+    seller_id: '-6',
+    id: '-1',
+    title: '$5 Nail Painting',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed ipsum nisl',
     active: true,
-    id: 2
+    category: {
+      shortname: 'beauty',
+      name: 'Beauty',
+      active: true,
+      id: 2
+    }
+  },
+  postPosting: {
+    seller_id: '-5',
+    id: null, //determined in test
+    title: 'This is a test posting',
+    description:  'some posting description text',
+    active: true,
+    category: {
+      id: 3,
+      shortname: 'tutor',
+      name: 'Tutor',
+      active: true,
+    }
   }
 };
 
 describe('/posting', () => {
-  it('GET /posting/:id', ()=> {
+  it('GET /posting/:id', () => {
     const postingId = '-1';
     const posting = request.get(`${API_PATH}posting/${postingId}`).then(JSON.parse);
-    return expect(posting).to.eventually.deep.equal(expectedPosting);
+    return expect(posting).to.eventually.deep.equal(expected.getPosting);
+  });
+
+  it('POST /posting/', () => {
+    const posting = request.post({
+      url: `${API_PATH}posting/`,
+      json: true,
+      headers: {'Content-Type': 'application/json'},
+      body: testData.createPosting
+    }).then(posting => {
+      expected.postPosting.id = posting.id;
+      expected.postPosting.createdAt = posting.createdAt;
+      expected.postPosting.updatedAt = posting.updatedAt;
+      if(!posting.id) {
+        throw new Error('posting did not return with ID');
+      }
+
+      return posting;
+    });
+
+    return expect(posting).to.eventually.deep.equal(expected.postPosting);
   });
 });

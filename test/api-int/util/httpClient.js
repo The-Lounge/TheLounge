5553ab5855442fc2eea05d4a13d955d372244e83
Greg Rozmarynowycz <crashtest1@live.com>
2017-04-22 14:25:10 -0400

    create test http client and utils index

---
+++ test/api-int/util/httpClient.js
@@ -0,0 +1,38 @@
+/**
+ * @author Greg Rozmarynowycz <greg@thunderlab.net>
+ */
+require('any-promise/register/q');
+const request = require('request-promise-any');
+
+class HttpClient {
+  /**
+   * @param params
+   * @param [params.baseUrl {string}]
+   */
+  constructor(params) {
+    this.jar = request.jar();
+    this._request = request.defaults({
+      jar: this.jar,
+      json: true,
+      baseUrl: params.baseUrl,
+    });
+  }
+
+  request(options) {
+    return this._request(options);
+  }
+
+  authenticate(url, credentials) {
+    return this._request.post({url, body: credentials});
+  }
+
+  get(url) {
+    return this._request.get({url});
+  }
+
+  post(url, body) {
+    return this._request.post({url, body});
+  }
+}
+
+module.exports = {HttpClient};

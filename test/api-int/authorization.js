2eb75d381f61b247ab9f89b2b273bcac852e8205
Greg Rozmarynowycz <crashtest1@live.com>
2017-04-22 15:07:31 -0400

    fix tests, add test for me endpoint

--- test/api-int/authorization.js
+++ test/api-int/authorization.js
@@ -2,14 +2,13 @@
  * @author Greg Rozmarynowycz <greg@thunderlab.net>
  */
 require('any-promise/register/q');
-const request = require('request-promise-any');
-const url = require('url');
 const chai = require('chai');
 const config = require('./sharedConfig');
 chai.use(require('chai-as-promised'));
 
 const expect = chai.expect;
 const util = require('./util');
+
 let httpClient = null;
 const endpoint = {
   POSTING: 'posting',
@@ -23,7 +22,7 @@ describe('authorization policies', () => {
   it('send a 401 Unauthorized response if client is not authorized to access an endpoint', () => {
     // Try to create a posting w/ authorization
     const createOp = httpClient.post(endpoint.POSTING, {});
-    expect(createOp).to.be.rejectedWith(Error, '401');
+    return expect(createOp).to.be.rejectedWith(Error, '401');
   });
 
   // no valid end points test...
--- test/api-int/client.js
+++ test/api-int/client.js
@@ -13,6 +13,10 @@ const util = require('./util');
 
 const Q = require('q');
 
+const endpoint = {
+  ME: 'me',
+};
+
 const loginOptions = {
   url: url.resolve(config.API_PATH, config.endpoint.LOGIN),
   json: true,
@@ -48,36 +52,56 @@ const expected = {
   },
 };
 
-describe('/login', () => {
-  it('responds to a POST request', () => {
-    const loginReq = request.post(Object.assign({body: {}}, loginOptions));
-    return expect(loginReq).to.be.rejectedWith(Error, '400');
+describe('ClientController', () => {
+  describe('/login', () => {
+    it('responds to a POST request', () => {
+      const loginReq = request.post(Object.assign({body: {}}, loginOptions));
+      return expect(loginReq).to.be.rejectedWith(Error, '400');
+    });
+
+    it('responds with user details (200) when provided with valid credentials', () => {
+      const loginReq = request.post(Object.assign({body: config.validLogin}, loginOptions))
+        .then(util.stripMetaDates);
+      return expect(loginReq).to.eventually.deep.equal(expected.user);
+    });
+
+    it('responds with a 400 code when provided malformed body', () => {
+      const loginReq = request.post(Object.assign({body: {not: 'valid'}}, loginOptions));
+      return expect(loginReq).to.be.rejectedWith(Error, '400');
+    });
+
+    it('responds with a 401 code when provided invalid credentials', () => {
+      const loginReq1 = request.post(Object.assign({body: testData.invalidLogin_un}, loginOptions));
+      const loginReq2 = request.post(Object.assign({body: testData.invalidLogin_pw}, loginOptions));
+
+      return Q.all([
+        expect(loginReq1).to.be.rejectedWith(Error, '401'),
+        expect(loginReq2).to.be.rejectedWith(Error, '401')]);
+    });
   });
 
-  it('responds with user details (200) when provided with valid credentials', () => {
-    const loginReq = request.post(Object.assign({body: config.validLogin}, loginOptions))
-      .then(util.stripMetaDates);
-    return expect(loginReq).to.eventually.deep.equal(expected.user);
+  describe('/logout', () => {
+    it('responds to a GET request', () => {
+      const logoutReq = request.get(logoutOptions);
+      return expect(logoutReq).to.eventually.have.property('statusCode', 200);
+    });
   });
 
-  it('responds with a 400 code when provided malformed body', () => {
-    const loginReq = request.post(Object.assign({body: {not: 'valid'}}, loginOptions));
-    return expect(loginReq).to.be.rejectedWith(Error, '400');
-  });
+  describe('/me', () => {
+    before(function before() {
+      this.httpClient = new util.HttpClient({baseUrl: config.API_PATH});
+    });
 
-  it('responds with a 401 code when provided invalid credentials', () => {
-    const loginReq1 = request.post(Object.assign({body: testData.invalidLogin_un}, loginOptions));
-    const loginReq2 = request.post(Object.assign({body: testData.invalidLogin_pw}, loginOptions));
+    it('responds with a 401 if the client is unauthorized', function meUnauth() {
+      const meReq = this.httpClient.get(endpoint.ME);
+      return expect(meReq).to.be.rejectedWith(Error, '401');
+    });
 
-    return Q.all([
-      expect(loginReq1).to.be.rejectedWith(Error, '401'),
-      expect(loginReq2).to.be.rejectedWith(Error, '401')]);
-  });
-});
-
-describe('/logout', () => {
-  it('responds to a GET request', () => {
-    const logoutReq = request.get(logoutOptions);
-    return expect(logoutReq).to.eventually.have.property('statusCode', 200);
+    it('responds with the currently authenticated user', function meAuth() {
+      // TODO: THIS WILL BE AUTHENTICATED FOR ANY SUBSEQUENT TESTS
+      const meReq = this.httpClient.authenticate(config.endpoint.LOGIN, config.validLogin)
+        .then(() => this.httpClient.get(endpoint.ME));
+      return expect(meReq).to.eventually.have.property('userName', config.validLogin.userName);
+    });
   });
 });
